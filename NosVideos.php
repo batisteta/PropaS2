e31b2f307e06fab3b0487e3e0166752eebfdedc9
batisteta <34096625+batisteta@users.noreply.github.com>
2018-02-06 14:55:53 +0100

    Create NosVideos.php

---
+++ NosVideos.php
@@ -0,0 +1,28 @@
+ <!DOCTYPE html>
+<html>
+	<head>
+		<title> Propagande De La Viande </title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+		<body>
+		<nav>
+			<ul> 
+				<li class="menu-accueil"><a href="accueil.php">Accueil</a></li>
+				<li class="menu-NosVideos"><a href="NosVideos.php">Tout Savoir</a>
+					<ul class="submenu">
+						<li><a href="NosVideos.php">Tout savoir en video!</a></li>
+						<li><a href="Viande.php">Tout savoir en image!</a></li>
+					</ul></li>
+				<li class="menu-contact"><a href="Contact.php">Nous contacter</a></li>
+			</ul>
+		</nav>
+			<center>	
+				<video width="400" controls>
+				<source src="VideoViande.mp4" type="video/mp4">
+				<source src="videoViande.ogg" type="video/ogg">
+				Votre navigateur ne supporte pas le format vidéo.
+				</video>
+			</center>
+		</body>
+</html> 
