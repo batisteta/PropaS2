841f426cf6b67ea0cbed2d563ddf24a40ba6034a
batisteta <34096625+batisteta@users.noreply.github.com>
2018-02-06 15:11:00 +0100

    Create Viande.php

---
+++ Viande.php
@@ -0,0 +1,33 @@
+ <!DOCTYPE html>
+<html>
+	<head>
+		<title>Les Bienfaits de la viande</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="style.css">
+	</head>
+		<body>
+		<nav>
+			<ul> 
+				<li class="menu-accueil"><a href="accueil.php">Accueil</a></li>
+				<li class="menu-NosVideos"><a href="NosVideos.php">Tout Savoir</a>
+					<ul class="submenu">
+						<li><a href="NosVideos.php">Tout savoir en video!</a></li>
+						<li><a href="Viande.php">Tout savoir en image!</a></li>
+					</ul></li>
+				<li class="menu-contact"><a href="Contact.php">Nous contacter</a></li>
+			</ul>
+		</nav>
+		<div>
+			<h1>Les bienfaits de la viande.</h1>
+			<center> <img src="viande.JFIF" alt="Un steack"> </center>
+		
+			<center><p>La viande occupe une place centrale dans notre alimentation : <br />
+			entrant dans la préparation de multiples recettes des plus traditionnelles aux plus actuelles,<br />
+			elle est liée à des moments de plaisir et de fête, en famille ou entre amis.
+			<br />Mais la viande joue également un rôle prédominant dans l’équilibre alimentaire,<br />
+			grâce à sa grande richesse en éléments essentiels à la croissance et au maintien en bonne santé.<br /></p>
+			<p> <font color="red">Si vous êtes intéressés par des informations plus poussées sur la viande, vous pouvez cliquer sur le lien ci-dessous:</font></p> </center>
+			<center><a href="https://www.bigard.fr/fr/bienfaits-de-la-viande.html">La viande, c'est bon!</a></center>
+			</div>
+	</body>
+</html>
